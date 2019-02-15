cd5a1c27db8482d9dbb704033bcbd4403ebc87be
caoyq0521 <704729283@qq.com>
2019-02-15 10:47:55 +0800

    测试ios

---
+++ widget/script/basic.js
@@ -0,0 +1,20 @@
+const SERVER_URL = 'http://192.168.0.226:8909/api/V1.0';
+
+function post(url, data, successCallback) {
+    api.ajax({
+        headers: {
+            "X-APICloud-AppId": "A6005584828914",
+            "X-APICloud-AppKey": "FCD9627E-F21A-8BA8-367A-21ADDD4A3820"
+        },
+        url: SERVER_URL + url,
+        method: 'post',
+        data: data,
+    }, function(ret, err) {
+        if (ret) {
+            successCallback(ret)
+        } else {
+            alert(JSON.stringify(err));
+        }
+    });
+
+}
